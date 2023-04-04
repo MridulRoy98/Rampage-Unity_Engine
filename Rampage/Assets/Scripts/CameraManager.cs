127a664bd220fa00a3c11fc5b3826bca314fcbdf
Mridul Roy <mridulroy23@gmail.com>
2023-04-05 02:15:58 +0600

    Camera movement done

--- Rampage/Assets/Scripts/CameraManager.cs
+++ Rampage/Assets/Scripts/CameraManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float offsetAmount = 0.5f;
+    [SerializeField] private int offsetSpeed = 3;
+    [SerializeField] private Vector3 cameraOffset;
     void Update()
     {
-        
+        transform.position = new Vector3(cameraOffset.x, cameraOffset.y, transform.position.z - (offsetAmount * offsetSpeed * Time.deltaTime));
     }
 }
