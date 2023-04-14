b0611d46bdd940373ff50c3220c59dc0e4d81302
Mridul Roy <mridulroy23@gmail.com>
2023-04-15 03:09:20 +0600

    Zombies Can follow but not player

--- Rampage/Assets/Scripts/ZombieSpawner.cs
+++ Rampage/Assets/Scripts/ZombieSpawner.cs
@@ -1,18 +1,34 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieSpawner : MonoBehaviour
 {
     [SerializeField] private List<GameObject> zombies;
-
+    [SerializeField] private List<GameObject> spawned;
+    [SerializeField] private GameObject player;
     private void Start()
     {
+
+        spawned = new List<GameObject>();
         SpawnZombies();
     }
-
+    private void Update()
+    {
+        FollowPlayer();
+    }
     private void SpawnZombies()
     {
         int randomIndex = Random.Range(0, zombies.Count);
-        Instantiate(zombies[randomIndex], this.transform.position, Quaternion.identity);
+        GameObject spawnedZombie = Instantiate(zombies[randomIndex], this.transform.position, Quaternion.identity);
+        spawned.Add(spawnedZombie);
+    }
+
+    private void FollowPlayer()
+    {
+        foreach( var zombie in spawned)
+        {
+            zombie.GetComponent<NavMeshAgent>().SetDestination(Vector3.zero);
+        }
     }
 }
