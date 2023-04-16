c70d9a571227e6cfa4bd7ab223f92318aeb201b1
Mridul Roy <mridulroy23@gmail.com>
2023-04-17 03:12:52 +0600

    Zombie Animations Set up

--- Rampage/Assets/Scripts/ZombieMovement.cs
+++ Rampage/Assets/Scripts/ZombieMovement.cs
@@ -7,11 +7,16 @@ public class ZombieMovement : MonoBehaviour
     PlayerMovement playermovement;
 
     private NavMeshAgent agent;
+    private Animator zombieAnimator;
+    private float rotationSpeed = 1f;
     void Start()
     {
+        zombieAnimator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Character");
         playermovement = player.GetComponent<PlayerMovement>();
+        chasingAnimation();
+        agent.speed = 0.001f;
     }
     void Update()
     {
@@ -20,5 +25,26 @@ public class ZombieMovement : MonoBehaviour
     private void FollowPlayer()
     {
         agent.SetDestination(playermovement.GetPlayerPosition());
+
+        Vector3 lookDirection = playermovement.GetPlayerPosition() - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
+    private void chasingAnimation()
+    {
+        int animNumber = Random.Range(0, 2);
+
+        switch (animNumber)
+        {
+            case 0:
+                zombieAnimator.SetBool("zombie_run", true);
+                break;
+            case 1:
+                zombieAnimator.SetBool("zombie_crawl", true);
+                break;
+        }
+
+    }
+
+
 }
