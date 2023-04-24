947aa4165153398755542e9d8c97b61250dae3a8
Mridul Roy <mridulroy23@gmail.com>
2023-04-24 14:54:14 +0600

    Animator Bool Issue Solved

--- Rampage/Assets/Scripts/PlayerMovement.cs
+++ Rampage/Assets/Scripts/PlayerMovement.cs
@@ -2,19 +2,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Animator playerAnimator;
     private CharacterController cc;
 
     [Header("Character Movement Stats")]
+    
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float rotateSpeed = 10f;
-
-    private Vector3 moveDirection;
+    [SerializeField] private Animator playerAnimator;
 
     void Start()
     {
         cc = GetComponent<CharacterController>();
-        playerAnimator = GetComponentInChildren<Animator>();
+        //playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -27,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
 
     }
     private void Move()
-    {   
+    {
         //Get Player Input
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -40,15 +39,20 @@ public class PlayerMovement : MonoBehaviour
         if (moveDirection != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation , rotateSpeed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed);
         }
 
         //Trigger Animation
-        if(moveDirection != Vector3.zero)
+        if (moveDirection != Vector3.zero)
         {
             playerAnimator.SetBool("isRunning", true);
+            Debug.Log("true");
         }
-        else playerAnimator.SetBool("isRunning", false);
+        else
+        {
+            playerAnimator.SetBool("isRunning", false);
+            Debug.Log("false");
+        }
     }
 
     public Vector3 GetPlayerPosition()
